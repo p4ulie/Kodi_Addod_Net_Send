d01912dfaef06f16c03d9ba566fb291c35b015a8
Pavol Antalík <p.antalik@necaso.com>
2017-03-28 20:56:23 +0200

    Third group for sending commands, settings for time and events

--- service.network.send/service.py
+++ service.network.send/service.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import time
+import datetime
 import xbmc
 import xbmcgui
 import xbmcaddon
@@ -12,8 +13,9 @@ addonname = addon.getAddonInfo('name')
 class Settings_Monitor(xbmc.Monitor):
     def __init__(self, player):
         self.player = player
-        self.movie_playing = None
-        self.tv_show_playing = None
+        self.playing_movie = None
+        self.playing_tv_show = None
+        self.playing_other = None
 
     def onSettingsChanged(self):
         self.player.update_settings()
@@ -25,45 +27,7 @@ class Player_Monitor(xbmc.Player):
         self.tv_show_playing = None
         self.other_playing = None
 
-    def send_command_on(self):
-
-        xbmc.log("%s: Toggle Group 01: %s" % (addonname, self.enabled_group_01_toggle), level=xbmc.LOGNOTICE)
-        xbmc.log("%s: Toggle Group 02: %s" % (addonname, self.enabled_group_02_toggle), level=xbmc.LOGNOTICE)
-        xbmc.log("%s: HTTP Method 01: %s" % (addonname, self.http_method_01), level=xbmc.LOGNOTICE)
-        xbmc.log("%s: HTTP Method 02: %s" % (addonname, self.http_method_02), level=xbmc.LOGNOTICE)
-
-        if self.enabled_group_01_toggle == "true":
-            if self.url_list_01 != "":
-                url_list = self.url_list_01.split(';')
-                for url in url_list:
-                    if self.http_method_01 == "GET":
-                        r = requests.get(url, data=self.command_on_01)
-                        xbmc.log("%s: Group 01, sending GET %s to %s" % (addonname, self.command_on_01, url),
-                                 level=xbmc.LOGNOTICE)
-                    if self.http_method_01 == "POST":
-                        r = requests.post(url, data=self.command_on_01)
-                        xbmc.log("%s: Group 01, sending POST %s to %s" % (addonname, self.command_on_01, url),
-                                 level=xbmc.LOGNOTICE)
-                    if self.http_method_01 == "PUT":
-                        r = requests.put(url, data=self.command_on_01)
-                        xbmc.log("%s: Group 01, sending PUT %s to %s" % (addonname, self.command_on_01, url),
-                                 level=xbmc.LOGNOTICE)
-
-        if self.enabled_group_02_toggle == "true":
-            if self.url_list_02 != "":
-                url_list = self.url_list_02.split(';')
-                for url in url_list:
-                    xbmc.log("%s: Group 02, sending %s to %s" % (addonname, self.command_on_02, url), level=xbmc.LOGNOTICE)
-                    r = requests.put(url, data=self.command_on_02)
-                    if self.http_method_02 == "GET":
-                        r = requests.get(url, data=self.command_on_02)
-                    if self.http_method_02 == "POST":
-                        r = requests.post(url, data=self.command_on_02)
-                    if self.http_method_02 == "PUT":
-                        r = requests.put(url, data=self.command_on_02)
-
-
-    def send_command_off(self):
+    def send_command_on(self, event):
 
         xbmc.log("%s: Toggle Group 01: %s" % (addonname, self.enabled_group_01_toggle), level=xbmc.LOGNOTICE)
         xbmc.log("%s: Toggle Group 02: %s" % (addonname, self.enabled_group_02_toggle), level=xbmc.LOGNOTICE)
@@ -73,102 +37,155 @@ class Player_Monitor(xbmc.Player):
         xbmc.log("%s: Toggle TV Shows: %s" % (addonname, self.active_for_tvshows_toggle), level=xbmc.LOGNOTICE)
         xbmc.log("%s: Toggle Other: %s" % (addonname, self.active_for_other_toggle), level=xbmc.LOGNOTICE)
 
-        if self.enabled_group_01_toggle == "true":
-            if self.url_list_01 != "":
-                url_list = self.url_list_01.split(';')
-                for url in url_list:
-                    if self.http_method_01 == "GET":
-                        r = requests.get(url, data=self.command_off_01)
-                        xbmc.log("%s: Group 01, sending GET %s to %s" % (addonname, self.command_off_01, url),
-                                 level=xbmc.LOGNOTICE)
-                    if self.http_method_01 == "POST":
-                        r = requests.post(url, data=self.command_off_01)
-                        xbmc.log("%s: Group 01, sending POST %s to %s" % (addonname, self.command_off_01, url),
-                                 level=xbmc.LOGNOTICE)
-                    if self.http_method_01 == "PUT":
-                        r = requests.put(url, data=self.command_off_01)
-                        xbmc.log("%s: Group 01, sending PUT %s to %s" % (addonname, self.command_off_01, url),
-                                 level=xbmc.LOGNOTICE)
+        if (self.active_for_movies_toggle and self.playing_movie)\
+        or (self.active_for_tvshows_toggle and self.playing_tv_show)\
+        or (self.active_for_other_toggle and self.playing_other):
 
-        if self.enabled_group_02_toggle == "true":
-            if self.url_list_02 != "":
-                url_list = self.url_list_02.split(';')
-                for url in url_list:
-                    xbmc.log("%s: Group 02, sending %s to %s" % (addonname, self.command_off_02, url), level=xbmc.LOGNOTICE)
-                    if self.http_method_02 == "GET":
-                        r = requests.get(url, data=self.command_off_02)
-                    if self.http_method_02 == "POST":
-                        r = requests.post(url, data=self.command_off_02)
-                    if self.http_method_02 == "PUT":
-                        r = requests.put(url, data=self.command_off_02)
+            now = time.strptime(datetime.datetime.now().strftime("%H:%M"), "%H:%M")
+
+            if (self.active_time_from < now) or (self.active_time_to > now):
+
+
+                if self.enabled_group_01_toggle == "true":
+                    if self.url_list_01 != "":
+                        url_list = self.url_list_01.split(';')
+                        for url in url_list:
+                            xbmc.log("%s: Group 01, sending %s %s to %s" % (addonname, self.http_method_01, self.command_on_01, url),
+                                     level=xbmc.LOGNOTICE)
+                            if self.http_method_01 == "GET":
+                                r = requests.get(url, data=self.command_on_01)
+                            if self.http_method_01 == "POST":
+                                r = requests.post(url, data=self.command_on_01)
+                            if self.http_method_01 == "PUT":
+                                r = requests.put(url, data=self.command_on_01)
+
+                if self.enabled_group_02_toggle == "true":
+                    if self.url_list_02 != "":
+                        url_list = self.url_list_02.split(';')
+                        for url in url_list:
+                            xbmc.log("%s: Group 02, sending %s %s to %s" % (addonname, self.http_method_02, self.command_on_02, url),
+                                     level=xbmc.LOGNOTICE)
+                            if self.http_method_02 == "GET":
+                                r = requests.get(url, data=self.command_on_02)
+                            if self.http_method_02 == "POST":
+                                r = requests.post(url, data=self.command_on_02)
+                            if self.http_method_02 == "PUT":
+                                r = requests.put(url, data=self.command_on_02)
+
+                if self.enabled_group_03_toggle == "true":
+                    if self.url_list_03 != "":
+                        url_list = self.url_list_03.split(';')
+                        for url in url_list:
+                            xbmc.log("%s: Group 03, sending %s %s to %s" % (addonname, self.http_method_03, self.command_on_03, url),
+                                     level=xbmc.LOGNOTICE)
+                            if self.http_method_03 == "GET":
+                                r = requests.get(url, data=self.command_on_03)
+                            if self.http_method_03 == "POST":
+                                r = requests.post(url, data=self.command_on_03)
+                            if self.http_method_03 == "PUT":
+                                r = requests.put(url, data=self.command_on_03)
+
+
+    def send_command_off(self, event):
+
+        xbmc.log("%s: Toggle Group 01: %s" % (addonname, self.enabled_group_01_toggle), level=xbmc.LOGNOTICE)
+        xbmc.log("%s: Toggle Group 02: %s" % (addonname, self.enabled_group_02_toggle), level=xbmc.LOGNOTICE)
+        xbmc.log("%s: HTTP Method 01: %s" % (addonname, self.http_method_01), level=xbmc.LOGNOTICE)
+        xbmc.log("%s: HTTP Method 02: %s" % (addonname, self.http_method_02), level=xbmc.LOGNOTICE)
+        xbmc.log("%s: Toggle Movies: %s" % (addonname, self.active_for_movies_toggle), level=xbmc.LOGNOTICE)
+        xbmc.log("%s: Toggle TV Shows: %s" % (addonname, self.active_for_tvshows_toggle), level=xbmc.LOGNOTICE)
+        xbmc.log("%s: Toggle Other: %s" % (addonname, self.active_for_other_toggle), level=xbmc.LOGNOTICE)
+
+        if (self.active_for_movies_toggle and self.playing_movie)\
+        or (self.active_for_tvshows_toggle and self.playing_tv_show)\
+        or (self.active_for_other_toggle and self.playing_other):
+
+            now = time.strptime(datetime.datetime.now().strftime("%H:%M"), "%H:%M")
+
+            if (self.active_time_from < now) or (self.active_time_to > now):
+
+                if self.enabled_group_01_toggle == "true":
+                    if self.url_list_01 != "":
+                        url_list = self.url_list_01.split(';')
+                        for url in url_list:
+                            xbmc.log("%s: Group 01, sending %s %s to %s" % (addonname, self.http_method_01, self.command_off_01, url),
+                                     level=xbmc.LOGNOTICE)
+                            if self.http_method_01 == "GET":
+                                r = requests.get(url, data=self.command_off_01)
+                            if self.http_method_01 == "POST":
+                                r = requests.post(url, data=self.command_off_01)
+                            if self.http_method_01 == "PUT":
+                                r = requests.put(url, data=self.command_off_01)
+
+                if self.enabled_group_02_toggle == "true":
+                    if self.url_list_02 != "":
+                        url_list = self.url_list_02.split(';')
+                        for url in url_list:
+                            xbmc.log("%s: Group 02, sending %s %s to %s" % (addonname, self.http_method_02, self.command_off_02, url),
+                                     level=xbmc.LOGNOTICE)
+                            if self.http_method_02 == "GET":
+                                r = requests.get(url, data=self.command_off_02)
+                            if self.http_method_02 == "POST":
+                                r = requests.post(url, data=self.command_off_02)
+                            if self.http_method_02 == "PUT":
+                                r = requests.put(url, data=self.command_off_02)
+
+                if self.enabled_group_03_toggle == "true":
+                    if self.url_list_03 != "":
+                        url_list = self.url_list_03.split(';')
+                        for url in url_list:
+                            xbmc.log("%s: Group 03, sending %s %s to %s" % (addonname, self.http_method_03, self.command_off_03, url),
+                                     level=xbmc.LOGNOTICE)
+                            if self.http_method_03 == "GET":
+                                r = requests.get(url, data=self.command_off_03)
+                            if self.http_method_03 == "POST":
+                                r = requests.post(url, data=self.command_off_03)
+                            if self.http_method_03 == "PUT":
+                                r = requests.put(url, data=self.command_off_03)
+
 
     def onPlayBackStarted(self):
         # turn light switch off
 
         xbmc.log("NetSend addon - Playback started at %s" % time.time(), level=xbmc.LOGNOTICE)
 
-        if self.enabled_global_toggle == "true":
+        if self.enabled_global_toggle == "true" and self.active_on_play_toggle == "true":
             self.now_playing()
-            if self.movie_playing and self.active_for_movies_toggle == "true":
-                self.send_command_off()
-            if self.tv_show_playing and self.active_for_tvshows_toggle == "true":
-                self.send_command_off()
-            if self.other_playing and self.active_for_other_toggle == "true":
-                self.send_command_off()
+            self.send_command_off(event="Start")
 
     def onPlayBackStopped(self):
         # turn light switch on
 
         xbmc.log("NetSend addon - Playback stopped at %s" % time.time(), level=xbmc.LOGNOTICE)
 
-        if self.enabled_global_toggle == "true":
-            if self.movie_playing and self.active_for_movies_toggle == "true":
-                self.send_command_on()
-            if self.tv_show_playing and self.active_for_tvshows_toggle == "true":
-                self.send_command_on()
-            if self.other_playing and self.active_for_other_toggle == "true":
-                self.send_command_on()
+        if self.enabled_global_toggle == "true" and self.active_on_stop_toggle == "true":
+            self.send_command_on(event="Stop")
 
     def onPlayBackEnded(self):
         # turn light switch on
 
         xbmc.log("NetSend addon - Playback ended at %s" % time.time(), level=xbmc.LOGNOTICE)
 
-        if self.enabled_global_toggle == "true":
-            if self.movie_playing and self.active_for_movies_toggle == "true":
-                self.send_command_on()
-            if self.tv_show_playing and self.active_for_tvshows_toggle == "true":
-                self.send_command_on()
-            if self.other_playing and self.active_for_other_toggle == "true":
-                self.send_command_on()
+        if self.enabled_global_toggle == "true" and self.active_on_end_toggle == "true":
+            self.send_command_on(event="End")
 
     def onPlayBackPaused(self):
         # turn light switch on
 
         xbmc.log("NetSend addon - Playback paused at %s" % time.time(), level=xbmc.LOGNOTICE)
 
-        if self.enabled_global_toggle == "true":
+        if self.enabled_global_toggle == "true" and self.active_on_pause_toggle == "true":
             self.now_playing()
-            if self.movie_playing and self.active_for_movies_toggle == "true":
-                self.send_command_on()
-            if self.tv_show_playing and self.active_for_tvshows_toggle == "true":
-                self.send_command_on()
-            if self.other_playing and self.active_for_other_toggle == "true":
-                self.send_command_on()
+            self.send_command_on(event="Pause")
 
     def onPlayBackResumed(self):
         # turn light switch off
 
         xbmc.log("NetSend addon - Playback resumed at %s" % time.time(), level=xbmc.LOGNOTICE)
 
-        if self.enabled_global_toggle == "true":
+        if self.enabled_global_toggle == "true" and self.active_on_resume_toggle == "true":
             self.now_playing()
-            if self.movie_playing and self.active_for_movies_toggle == "true":
-                self.send_command_off()
-            if self.tv_show_playing and self.active_for_tvshows_toggle == "true":
-                self.send_command_off()
-            if self.other_playing and self.active_for_other_toggle == "true":
-                self.send_command_off()
+            self.send_command_off(event="Resume")
 
     def now_playing(self):
         # check media type when a video starts, pauses, or resumes
@@ -179,13 +196,13 @@ class Player_Monitor(xbmc.Player):
         xbmc.log("%s: Media info: %s" % (addonname, response_json), level=xbmc.LOGNOTICE)
 
         if response['result']['item']['type'] == 'movie':
-            self.movie_playing = True
-            self.tv_show_playing = False
-            self.other_playing = False
+            self.playing_movie = True
+            self.playing_tv_show = False
+            self.playing_other = False
         elif response['result']['item']['type'] == 'episode':
-            self.movie_playing = False
-            self.tv_show_playing = True
-            self.other_playing = False
+            self.playing_movie = False
+            self.playing_tv_show = True
+            self.playing_other = False
         else:
             self.movie_playing = False
             self.tv_show_playing = False
@@ -198,6 +215,15 @@ class Player_Monitor(xbmc.Player):
         self.active_for_tvshows_toggle = addon.getSetting('activeForTVshows_toggle')
         self.active_for_other_toggle = addon.getSetting('activeForOther_toggle')
 
+        self.active_on_play_toggle = addon.getSetting('activeOnPlay_toggle')
+        self.active_on_pause_toggle = addon.getSetting('activeOnPause_toggle')
+        self.active_on_resume_toggle = addon.getSetting('activeOnResume_toggle')
+        self.active_on_stop_toggle = addon.getSetting('activeOnStop_toggle')
+        self.active_on_end_toggle = addon.getSetting('activeOnEnd_toggle')
+
+        self.active_time_from = time.strptime(addon.getSetting('activeTimeFrom'), "%H:%M")
+        self.active_time_to = time.strptime(addon.getSetting('activeTimeTo'), "%H:%M")
+
         self.enabled_group_01_toggle = addon.getSetting('enabledGroup01_toggle')
         self.url_list_01 = addon.getSetting('urlList10')
         self.http_method_01 = addon.getSetting('httpMethod10')
@@ -209,6 +235,12 @@ class Player_Monitor(xbmc.Player):
         self.http_method_02 = addon.getSetting('httpMethod20')
         self.command_on_02 = addon.getSetting('command20On')
         self.command_off_02 = addon.getSetting('command20Off')
+
+        self.enabled_group_03_toggle = addon.getSetting('enabledGroup03_toggle')
+        self.url_list_03 = addon.getSetting('urlList30')
+        self.http_method_03 = addon.getSetting('httpMethod30')
+        self.command_on_03 = addon.getSetting('command30On')
+        self.command_off_03 = addon.getSetting('command30Off')
 
 
 def main():
